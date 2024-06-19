a4b826a7eae373559987a0c09b05f595de7743ed
Shumsz6 <167413992+Shumsz6@users.noreply.github.com>
2024-06-19 19:04:06 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDixxbxUlRlgEHVHbPSfl304tRs9JfVN1o",
+    authDomain: "crud-rosales.firebaseapp.com",
+    databaseURL: "https://crud-rosales-default-rtdb.firebaseio.com",
+    projectId: "crud-rosales",
+    storageBucket: "crud-rosales.appspot.com",
+    messagingSenderId: "219091188409",
+    appId: "1:219091188409:web:065501dbcdf58fd9f23968"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
